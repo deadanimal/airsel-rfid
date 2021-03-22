2737e0c12d361ce8403d83136a80fcf0f8070529
Nameyusliadiyusof <yusliadiyusof@pipeline.com.my>
2021-03-22 14:46:44 +0800

    change cron job setting

--- api/wams/crons/int11_planner_cron.py
+++ api/wams/crons/int11_planner_cron.py
@@ -3,27 +3,27 @@ import requests
 import pytz
 
 
-def int11_planner_cron():
+# def int11_planner_cron():
 
-    print('int11_planner_cron')
+print('int11_planner_cron')
 
-    # get timezone
-    timezone_ = pytz.timezone("Asia/Kuala_Lumpur")
+# get timezone
+timezone_ = pytz.timezone("Asia/Kuala_Lumpur")
 
-    # datetime object containing current date and time
-    now = datetime.now(timezone_)
+# datetime object containing current date and time
+now = datetime.now(timezone_)
 
-    print("now =", now)
+print("now =", now)
 
-    # dd/mm/YY H:M:S
-    from_date = now.strftime("%Y-%m-%dT%H:30:00+00:00")
-    to_date = (now + timedelta(hours=1)).strftime("%Y-%m-%dT%H:29:59+00:00")
+# dd/mm/YY H:M:S
+from_date = now.strftime("%Y-%m-%dT%H:30:00+00:00")
+to_date = (now + timedelta(hours=1)).strftime("%Y-%m-%dT%H:29:59+00:00")
 
-    payload = {
-        "service_name": "getPlanner",
-        "from_date": from_date,
-        "to_date": to_date
-    }
+payload = {
+    "service_name": "getPlanner",
+    "from_date": from_date,
+    "to_date": to_date
+}
 
-    r = requests.post(
-        "https://airsel-rfid-api.pipe.my/v1/wams/services/", json=payload)
+r = requests.post(
+    "https://airsel-rfid-api.pipe.my/v1/wams/services/", json=payload)
