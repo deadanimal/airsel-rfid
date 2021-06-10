c75844f72a6748bf7ddb54b7b281dbd0b7e1c139
iszmaelIbrahim <ismail.ibrahim@pipeline.com.my>
2021-06-10 10:32:01 +0800

    commit

---
+++ web/src/app/shared/services/work-request/work-request.model.ts
@@ -0,0 +1,32 @@
+export class WorkRequestModel {
+
+  id: any;
+  description: any;
+  long_description: any;
+  required_by_date: any;
+  approval_profile: any;
+  bo: any;
+  creation_datetime: any;
+  creation_user: any;
+  downtime_start: any;
+  planner: any;
+  work_class: any;
+  work_category: any;
+  work_priority: any;
+  requestor: any;
+  owning_access_group: any;
+  first_name: any;
+  last_name: any;
+  primary_phone: any;
+  mobile_phone: any;
+  home_phone: any;
+  node_id: any;
+  asset_id: any;
+  status: any;
+  int10_type: any;
+  work_request_id: any;
+  work_request_status: any;
+  created_date: any;
+  modified_date: any;
+
+}
