cd40bd8497213a92c03912ac4c280be3b5a6440c
quantmis <realhost01@gmail.com>
2021-07-26 12:56:39 +0800

    update on profile

--- web/src/app/auth/login/login.component.ts
+++ web/src/app/auth/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 import { Router } from '@angular/router';
 import swal from 'sweetalert2';
 
@@ -9,13 +9,19 @@ import { UsersService } from "src/app/shared/services/users/users.service";
 import { EmployeeService } from "src/app/shared/services/employee/employee.service";
 
 
+
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+
+  defaultPassword = 'air$elUs3r'
   loginForm: FormGroup;
+  submit_form: FormGroup;
+
 
   focus
   focus1
@@ -41,7 +47,7 @@ export class LoginComponent implements OnInit {
 
   }
   // deprecated
-  onSubmit() {
+  onSubmitOld() {
     let value = this.loginForm.value;
 
    
@@ -207,4 +213,239 @@ export class LoginComponent implements OnInit {
     //   })
     // }
   }
+
+  onSubmit() {
+
+    let loginBodyAD = {
+      service_name: "getActiveDirectory",
+      username: this.loginForm.value.emailForm,
+      password: this.loginForm.value.passwordForm
+    }
+
+    this.wamsService.getService(loginBodyAD).subscribe(
+      (resAD) => {
+        let staff_id = resAD['staff_no']
+        console.log("sid", staff_id);
+        if (resAD.status == "valid") {
+          this.employeeService
+            .filter("hr_employee_number=" + staff_id)
+            .subscribe(
+              (resEmp) => {
+
+                console.log("sini 2", resEmp)
+                console.log("resEmp[0].uuid", resEmp[0].uuid)
+                // to create user account in PIPE who AD is valid
+                // STEP 4
+                if (resEmp.length > 0) {
+                  console.log("sini 3")
+
+                  let bodyPIPE = {
+                    username: this.loginForm.value.username,
+                    email: resAD.email ? resAD.email : "",
+                    // email: '',
+                    password1: this.defaultPassword,
+                    password2: this.defaultPassword,
+                  };
+
+                  ////// check in table user whether the user exist or not
+                  this.userService.filter('employee_id=' + resEmp[0].uuid).subscribe(
+                    (resUserSer) => {
+                      console.log("resUserSer == ", resUserSer)
+                      // console.log("resUserSerid == ", resUserSer[0]['id'])
+
+                      //// if user not will create new user 
+                      if (resUserSer.length == 0) {
+                        console.log("acc not exist")
+
+                        console.log("bodyPIPE = ", bodyPIPE)
+                        this.authService.registerAccount(bodyPIPE).subscribe(
+                          (resPIPE) => {
+
+                            console.log("sini 4", resPIPE)
+
+                            if (resPIPE) {
+
+                              resAD["first_name"] = resAD.name;
+                              resAD["status"] = true;
+                              resAD["department"] = "";
+                              resAD["employee_id"] = resEmp[0].uuid;
+                              resAD["service_area"] = resAD.region;
+                              resAD["user_type"] = 'PL';
+
+                              this.userService
+                                .update(resAD, resPIPE.user.pk)
+                                .subscribe((resPIPE) => {
+                                  console.log("relogin => ", resPIPE)
+                                  this.retryLogin();
+
+                                });
+                            }
+                          },
+                          (err) => {
+                            console.error("err", err);
+                          }
+                        );
+
+                      } else { ////// if user exist will update the info
+                        console.log("acc exist")
+
+                        resAD["first_name"] = resAD.name;
+                        resAD["status"] = true;
+                        resAD["department"] = "";
+                        resAD["service_area"] = resAD.region;
+
+                        console.log("resUserSer['id']", resUserSer[0]['id'])
+                        this.userService
+                          .update(resUserSer[0]['id'], resAD)
+                          .subscribe(
+                            (resPIPE) => {
+                              console.log("relogin => ", resPIPE
+                              )
+                              this.retryLogin();
+
+                            });
+                      }
+
+
+                    },
+                    (errUserSer) => {
+                      console.log("errUserSer", errUserSer)
+                    }
+                  )
+                } else {
+                  alert("data not found")
+                }
+              },
+              (err) => {
+                console.error("err", err);
+              }
+            );
+        }
+        else {
+          this.onSubmitOld();
+          //   // to create user account in PIPE who AD is invalid
+          //   // STEP 5
+          //   let bodyPIPE = {
+          //     username: this.validations_form.value.username,
+          //     // email: "",
+          //     password1: this.validations_form.value.password,
+          //     password2: this.validations_form.value.password,
+          //   };
+          //   this.authService.registerAccount(bodyPIPE).subscribe(
+          //     (resPIPE) => {
+          //       this.retryLogin();
+          //     },
+          //     (err) => {
+          //       console.error("err", err);
+          //     }
+          //   );
+        }
+      },
+      (err) => {
+        console.error("err", err);
+      }
+    );
+
+    // //   },
+    // //   () => {
+    // //     // After
+    // //     // this.toastr.openToastr("Welcome back");
+    // //   }
+    // // );
+
+    /* if (
+      this.loginForm.username == "technical" ||
+      this.loginForm.username == "1"
+    ) {
+      // technical
+      this.router.navigate(["/technical/tabs/tab1"]);
+    } else if (
+      this.loginForm.username == "operator" ||
+      this.loginForm.username == "2"
+    ) {
+      // operator
+      this.router.navigate(["/operator/tabs/tab1"]);
+    } else if (
+      this.loginForm.username == "store-keeper" ||
+      this.loginForm.username == "3"
+    ) {
+      // inventory
+      this.router.navigate(["/store-keeper/tabs/tab1"]);
+    } else if (
+      this.loginForm.username == "store-supervisor" ||
+      this.loginForm.username == "4"
+    ) {
+      // inventory
+      this.router.navigate(["/store-supervisor/tabs/tab1"]);
+    } else {
+      alert("wrong user!");
+    } */
+  }
+  retryLogin() {
+    console.log("AAA", this.loginForm.value);
+
+    this.submit_form = this.formBuilder.group({
+      username: new FormControl(
+        this.loginForm.value.emailForm,
+        // "",
+      ),
+      password: new FormControl(
+        this.defaultPassword,
+        // "",
+      ),
+    });
+    console.log("AA", this.submit_form.value);
+    this.authService
+      .obtainToken(this.submit_form.value)
+      .subscribe(
+        (res) => {
+          // Success
+          // STEP 2
+          this.navigateByRole(this.authService.userType);
+        },
+        (err) => {
+          // Failed
+          // STEP 3
+          console.log("err", err);
+          let submitFormBackup = this.formBuilder.group({
+            username: new FormControl(
+              this.loginForm.value.emailForm,
+              // "",
+            ),
+            password: new FormControl(
+              this.loginForm.value.passwordForm,
+              // "",
+            ),
+          });
+
+          this.authService.obtainToken(submitFormBackup.value).subscribe(
+            (res) => {
+              this.navigateByRole(this.authService.userType);
+            },
+            (err) => {
+              console.log("err", err);
+
+            }
+          );
+
+        },
+        () => {
+        }
+      );
+  }
+
+  navigateByRole(userType: string) {
+    if (userType == "AM") {
+      this.router.navigate(['/ams/dashboard']);
+    } else {
+      this.router.navigate(['/planner/dashboard']);
+    }
+
+  }
+
+  // ultimatum
+
+
+
+
 }
