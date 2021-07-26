6f71936447ed1e146c291af41503ceb1ffebb684
iszmaelIbrahim <ismail.ibrahim@pipeline.com.my>
2021-07-26 09:20:59 +0800

    minor change on asset registration

---
+++ web/src/app/shared/services/maintenance-planner/maintenance-planner.model.ts
@@ -0,0 +1,10 @@
+export class MaintenancePlannerModel {
+  public id: string;
+  public planner: string;
+  public description: any;
+  public status: any;
+  public user_id: any;
+  public created_date: any;
+  public modified_date: any;
+  
+}
